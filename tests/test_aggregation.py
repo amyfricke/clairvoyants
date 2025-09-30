643142dd9ff6e657023e57341f5e5080828de5fc
amyfricke <amy.r.fricke@gmail.com>
2025-09-30 10:04:18 -0700

    Adding tests to clairvoyant

---
+++ tests/test_aggregation.py
@@ -0,0 +1,16 @@
+import pytest
+from clairvoyants import aggregation
+import pandas as pd
+
+# Example function to test: suppose aggregation.py has a function called sum_series
+# We'll write a test for it. If it doesn't exist, you can update the test later.
+
+
+def test_aggregate_to_longest():
+    data = pd.DataFrame({'dt': pd.date_range('2020-01-01',
+     periods=14, freq='D'), 'actual': [1, 1, 1, 1, 1, 1, 7, 2, 2,
+     2, 2, 2, 2, 14]})
+    result = aggregation.aggregate_to_longest(data)
+    assert all(result['period7'].actual == [13, 26])
+    assert all(result['period7'].dt == ['2020-01-07', 
+      '2020-01-14'])
